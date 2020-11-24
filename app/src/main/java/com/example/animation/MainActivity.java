2d1941f43a9d715a4168b49ae9405c4ad7cb8a75
Charity Howell <caccheme@hotmail.com>
2020-11-24 05:27:21 -0600

    Define View animation with xml

--- app/src/main/java/com/example/animation/MainActivity.java
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -2,24 +2,42 @@ package com.example.animation;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageView = findViewById(R.id.faceIcon);
+        imageView = findViewById(R.id.faceIcon);
     }
 
     public void onButtonClick(View v) {
-    }
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.grow);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
 
-    public void onShrinkBtnClick(View v) {
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                imageView.setScaleX(2);
+                imageView.setScaleY(2);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        imageView.startAnimation(animation);
     }
 
 }
