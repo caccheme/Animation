eb125d9b48b8bc5885b1d098e594789515d6270d
Charity Howell <caccheme@hotmail.com>
2020-11-25 05:46:30 -0600

    Declare frame animations in xml, setup for frame/drawable animation

--- app/src/main/java/com/example/animation/MainActivity.java
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -17,40 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.faceIcon);
     }
 
-    public void onButtonClick(View v) {
-        imageView.animate()
-                .scaleX(2)
-                .scaleY(2)
-                .rotationX(180)
-                .rotationY(180)
-                .translationX(200)
-                .translationY(200)
-                .setDuration(2000)
-                //can set listener for animations on older devices, small fraction of devices now
-                .setListener(new Animator.AnimatorListener() {
-                    @Override
-                    public void onAnimationStart(Animator animator) {
-                        Log.i("Animation", "OnAnimationStart");
-                    }
+    public void onStartBtnClick(View v) {
+    }
 
-                    @Override
-                    public void onAnimationEnd(Animator animator) {
-                        Log.i("Animation", "OnAnimationEnd");
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animator) {
-                        Log.i("Animation", "OnAnimationCancel");
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animator animator) {
-                        Log.i("Animation", "OnAnimationRepeat");
-                    }
-                });
+    public void onStopBtnClick(View v) {
     }
 
 }
