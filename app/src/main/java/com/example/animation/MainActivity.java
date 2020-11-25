2aeede7cf10d6456857f42dfbb38fbcb7a8f3e70
Charity Howell <caccheme@hotmail.com>
2020-11-25 05:53:00 -0600

    Launch frame animation with Java, infinite loop with stop feature added

--- app/src/main/java/com/example/animation/MainActivity.java
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.animation;
 
 import android.animation.Animator;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,18 +12,32 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    private AnimationDrawable monkeyAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageView = findViewById(R.id.animation);
+        if (imageView == null) throw new AssertionError();
+
+        imageView.setVisibility(View.INVISIBLE);
+        imageView.setBackgroundResource(R.drawable.monkey_animation);
+
+        monkeyAnimation = (AnimationDrawable) imageView.getBackground();
     }
 
     public void onStartBtnClick(View v) {
+        imageView.setVisibility(View.VISIBLE);
+        if (monkeyAnimation.isRunning()) {
+            monkeyAnimation.stop();
+        }
+        monkeyAnimation.start();
     }
 
     public void onStopBtnClick(View v) {
+        monkeyAnimation.stop();
     }
 
 }
