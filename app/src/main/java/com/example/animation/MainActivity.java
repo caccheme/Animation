a954721784e57c1fd306c1c1dba7cf28ab7eab50
Charity Howell <caccheme@hotmail.com>
2020-11-25 05:33:25 -0600

    Define View animation with java, play around with animation features and capture code for older devices

--- app/src/main/java/com/example/animation/MainActivity.java
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.animation;
 
+import android.animation.Animator;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,7 +24,33 @@ public class MainActivity extends AppCompatActivity {
         imageView.animate()
                 .scaleX(2)
                 .scaleY(2)
-                .setDuration(2000);
+                .rotationX(180)
+                .rotationY(180)
+                .translationX(200)
+                .translationY(200)
+                .setDuration(2000)
+                //can set listener for animations on older devices, small fraction of devices now
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animator) {
+                        Log.i("Animation", "OnAnimationStart");
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        Log.i("Animation", "OnAnimationEnd");
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
+                        Log.i("Animation", "OnAnimationCancel");
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animator) {
+                        Log.i("Animation", "OnAnimationRepeat");
+                    }
+                });
     }
 
 }
