6c53948541b0064cedffc7b205740b271c8f560e
Charity Howell <caccheme@hotmail.com>
2020-11-27 06:02:54 -0600

    Set up to animate properties without listeners

--- app/src/main/java/com/example/animation/MainActivity.java
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -1,50 +1,24 @@
 package com.example.animation;
 
-import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ScrollView;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ScrollView mScroll;
-    private TextView mLog;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//      Initialize the logging components
-        mScroll = findViewById(R.id.scrollLog);
-        mLog = findViewById(R.id.tvLog);
-        mLog.setText("");
-
+        imageView = findViewById(R.id.faceIcon);
     }
 
-    public void onRunBtnClick(View v) {
-
-        ValueAnimator animator = ValueAnimator.ofFloat(1f, 20f)
-        .setDuration(2000);
-        animator.addUpdateListener(animation -> displayMessage("timestamp: " + animation.getCurrentPlayTime() + ", value: " +
-                animation.getAnimatedValue()));
-//        animator.setRepeatCount(ValueAnimator.INFINITE);
-        animator.setRepeatCount(2);
-        animator.setRepeatMode(ValueAnimator.REVERSE);
-        animator.start();
-
+    public void onButtonClick(View v) {
     }
 
-    public void onClearBtnClick(View v) {
-        mLog.setText("");
-        mScroll.scrollTo(0, mScroll.getBottom());
-    }
-
-    public void displayMessage(String message) {
-        mLog.append(message + "\n");
-        mScroll.scrollTo(0, mScroll.getBottom());
-    }
 }
