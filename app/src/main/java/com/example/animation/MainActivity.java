9843b4bdab81fd4932419406451563ceaaddff36
Charity Howell <caccheme@hotmail.com>
2020-11-27 06:09:55 -0600

    Animate properties without listeners, grow object in x and y direction simultaneously

--- app/src/main/java/com/example/animation/MainActivity.java
+++ app/src/main/java/com/example/animation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.animation;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -19,6 +20,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View v) {
+        //change x and y size of object simultaneously
+        ObjectAnimator animatorX = ObjectAnimator.ofFloat(imageView,
+                "scaleX", 1f, 2f)
+                .setDuration(1000);
+        animatorX.start();
+
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(imageView,
+                "scaleY", 1f, 2f)
+                .setDuration(1000);
+        animatorY.start();
+
     }
 
 }
